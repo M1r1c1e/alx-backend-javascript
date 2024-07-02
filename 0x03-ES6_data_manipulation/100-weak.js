914142b2eb793947c4882ee830667ba755c2891d
M1r1c1e <andersonmiracle@gmail.com>
2024-07-02 12:05:16 +0000

    javascript data manipulation

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,13 +1,14 @@
+/**
+ * Exporting a const instance of WeakMap and name it weakMap
+ */
 export const weakMap = new WeakMap();
-
+const MAX_ENDPOINT_CALLS = 5;
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 0);
   }
-
   weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-
-  if (weakMap.get(endpoint) >= 5) {
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
     throw new Error('Endpoint load is high');
   }
 }
